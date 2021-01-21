5bd4afb06a3e5da61ddc78813b7abd05f1a4d11e
Kamil Halko <k.halko@nomtek.com>
2021-01-21 21:30:29 +0100

    Implement resolving to the same URI

--- app/src/main/java/com/nomtek/exoplayercrash/media/CustomResolver.kt
+++ app/src/main/java/com/nomtek/exoplayercrash/media/CustomResolver.kt
@@ -6,9 +6,8 @@ import com.google.android.exoplayer2.upstream.ResolvingDataSource
 
 class CustomResolver : ResolvingDataSource.Resolver {
     override fun resolveDataSpec(dataSpec: DataSpec): DataSpec {
-        return DataSpec(
+        return dataSpec.withUri(
             Uri.parse("https://storageaudiobursts.blob.core.windows.net/stream/4c473e62-524d-4482-b2e6-36983b2fa25e.m3u8")
-//            Uri.parse("https://storage.googleapis.com/uamp/The_Kyoto_Connection_-_Wake_Up/01_-_Intro_-_The_Way_Of_Waking_Up_feat_Alan_Watts.mp3")
         )
     }
 }
